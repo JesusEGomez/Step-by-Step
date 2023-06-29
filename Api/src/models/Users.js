b261b25f63eb904cc66eafb5b6762baa411f93cf
lgramajo99 <lucianongramajo@gmail.com>
2023-06-28 21:34:06 -0300

    model user listo para revision

--- Api/src/models/Users.js
+++ Api/src/models/Users.js
@@ -1,14 +1,83 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define("user", {
+    sequelize.define("users", {
         id: {
             type: DataTypes.INTEGER,
             autoIncrement: true,
             primaryKey: true,
-        }, name: {
+        },
+        name: {
             type: DataTypes.STRING,
             allowNull: false,
-        }
+            validate: {
+                len: [5, 50],
+            },
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5, 50],
+            },
+        },
+        user: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: {
+                    msg: "El campo usuario no puede estar vacío",
+                },
+            },
+        },
+        mail: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+            validate: {
+                isEmail: true,
+                validateEmailOrCellphone: function () {
+                    if (!this.mail && !this.phone) {
+                        throw new Error("Debe proporcionar al menos un email o un celular");
+                    }
+                },
+            },
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+            validate: {
+                is: /^[0-9+]+$/,
+                validateEmailOrCellphone: function () {
+                    if (!this.mail && !this.phone) {
+                        throw new Error("Debe proporcionar al menos un email o un celular");
+                    }
+                },
+                len: {
+                    args: [8, 12],
+                    msg: "La longitud del número de celular debe estar entre 8 y 12 caracteres",
+                },
+            },
+        },
+        isAdmin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        pass: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                is: {
+                    args: /^(?=.*[A-Z])(?=.*\d)(?=.*[a-zA-Z])(?=.*[\W_])/,
+                    msg: "La contraseña debe contener al menos una letra mayúscula, un número, letras en general y un carácter especial",
+                },
+                len: {
+                    args: [6],
+                    msg: "La contraseña debe tener al menos 6 caracteres",
+                },
+            },
+        },
     })
 }
