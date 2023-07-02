ad1cb5427814231bfe1505c21f9004f7d2d5d772
lgramajo99 <lucianongramajo@gmail.com>
2023-07-02 15:13:58 -0300

    Controllers de users listo.

--- Api/src/routes/users.routes.js
+++ Api/src/routes/users.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 
-const userAllCtrl = require('../controllers/users/usersAllCtrl.js')
+const userAllCtrl = require('../controllers/users/usersAllCtrl.js');
 const createUserCtrl = require('../controllers/users/createUsersCtrl.js');
-const deleteUserCtrl = require('../controllers/users/deleteUserCtrl.js')
-const userByIdCtrl = require('../controllers/users/userByIdCtrl.js')
+const deleteUserCtrl = require('../controllers/users/deleteUserCtrl.js');
+const userByIdCtrl = require('../controllers/users/userByIdCtrl.js');
+const updateUserCtrl = require('../controllers/users/updateUserCtrl.js');
 
 const usersRouter = Router();
 
@@ -11,5 +12,6 @@ usersRouter.post('/', createUserCtrl);
 usersRouter.get('/', userAllCtrl);
 usersRouter.delete('/:id', deleteUserCtrl);
 usersRouter.get('/:id', userByIdCtrl);
+usersRouter.put('/id', updateUserCtrl);
 
 module.exports = usersRouter;
