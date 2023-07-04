238e5026c922b009b9ce7b85f5c102f645925d12
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-07-04 17:33:30 -0300

    nada

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -4,17 +4,17 @@ const { Router } = require("express");
 
 const router = Router();
 // const productsRouter = require("./productsRoutes");
-const createProductCtrl = require('../controllers/products/productCreateCtrl');
-const usersRouter = require('../routes/users.routes.js')
-const addressRoutes = require('../routes/address.routes.js')
+const createProductCtrl = require("../controllers/products/productCreateCtrl");
+const usersRouter = require("../routes/users.routes.js");
+const addressRoutes = require("../routes/address.routes.js");
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-// router.use("/products", productsRouter);
+router.use("/products", productsRouter);
 
 // router.get('/users', userAllCtrl)
-router.post('/producto', createProductCtrl);
-router.use('/users', usersRouter);
-router.use('/address', addressRoutes);
+// router.post('/producto', createProductCtrl);
+router.use("/users", usersRouter);
+router.use("/address", addressRoutes);
 
 module.exports = router;
