816d44c4a1ed8957783675a603790b08116538a1
lgramajo99 <lucianongramajo@gmail.com>
2023-07-05 10:32:38 -0300

    trabajando con la relacion con imagenes

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,18 +1,12 @@
 const { Router } = require("express");
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
 const router = Router()
 
-const productsRouter = require("./productsRoutes");
-const usersRouter = require('../routes/users.routes.js')
-const addressRoutes = require('../routes/address.routes.js')
-
-// Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 // router.use("/products", productsRouter);
 const brandsRoutes = require("./brands.routes.js");
 const usersRouter = require("./users.routes");
 const addressRoutes = require("./address.routes");
+const productsRouter = require('./productsRoutes.js')
 
 router.use('/users', usersRouter);
 router.use('/address', addressRoutes);
