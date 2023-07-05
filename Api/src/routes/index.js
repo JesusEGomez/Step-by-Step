eebab4b56d2dae1e933cffe26738cc642850fed8
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-07-04 21:47:55 -0300

    gitflow-feature-stash: tienda

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,14 +1,8 @@
 const { Router } = require("express");
 
 const router = Router();
-// const productsRouter = require("./productsRoutes");
-const createProductCtrl = require("../controllers/products/productCreateCtrl");
-const usersRouter = require("../routes/users.routes.js");
-const addressRoutes = require("../routes/address.routes.js");
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-router.use("/products", productsRouter);
+const productsRouter = require("./productsRoutes");
 
 const usersRouter = require("./users.routes");
 const addressRoutes = require("./address.routes");
@@ -16,9 +10,6 @@ const addressRoutes = require("./address.routes");
 router.use("/products", productsRouter);
 
 // router.get('/users', userAllCtrl)
-// router.post('/producto', createProductCtrl);
-router.use("/users", usersRouter);
-router.use("/address", addressRoutes);
 
 router.use("/users", usersRouter);
 router.use("/address", addressRoutes);
