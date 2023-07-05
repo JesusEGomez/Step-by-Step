775b0d2c25556ed98bb6f0de3b682642372f50d6
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2023-07-05 12:37:51 -0300

    paginado

--- Client/src/features/productsSlice.js
+++ Client/src/features/productsSlice.js
@@ -2,8 +2,17 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 const GET_URL = "http://localhost:3001/products";
 
+const recorrerArray = (array, propiedad) => {
+  const newArray = [];
+  array.forEach((element) => {
+    newArray.push(element[propiedad]);
+  });
+  return newArray;
+};
+
 const initialState = {
   products: [],
+  currentPage: 1,
 };
 
 export const fetchProducts = createAsyncThunk(
@@ -21,13 +30,27 @@ export const fetchProducts = createAsyncThunk(
 export const productsSlice = createSlice({
   name: "products",
   initialState,
-  reducer: {},
+  reducers: {
+    setCurrentPage: (state, actions) => {
+      console.log(actions);
+      state.currentPage = actions.payload;
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(fetchProducts.fulfilled, (state, actions) => {
         actions.payload.forEach((element) => {
-          console.log(element);
-          state.products.push(element);
+          const categories = recorrerArray(element.categories, "name");
+          const colors = recorrerArray(element.colors, "color");
+          const images = recorrerArray(element.images, "imageUrl");
+          const sizes = recorrerArray(element.sizes, "size");
+          state.products.push({
+            ...element,
+            categories,
+            colors,
+            images,
+            sizes,
+          });
         });
       })
       .addCase(fetchProducts.rejected, (state, actions) => {
@@ -36,5 +59,6 @@ export const productsSlice = createSlice({
   },
 });
 export const getAllProducts = (state) => state.products.products;
-export const {} = productsSlice.actions;
+export const getCurrentPage = (state) => state.products.currentPage;
+export const { setCurrentPage } = productsSlice.actions;
 export default productsSlice.reducer;
