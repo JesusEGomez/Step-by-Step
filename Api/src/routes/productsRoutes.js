5cc6c297f1ddf488d9eaef53c8bf0041160c0484
ClaudioCabrera06 <claudiocabrera2001@gmail.com>
2023-07-04 22:43:25 -0300

    errores

--- Api/src/routes/productsRoutes.js
+++ Api/src/routes/productsRoutes.js
@@ -1,12 +1,12 @@
 const { Router } = require("express");
-// const { getProducts, createProduct } = require("../handlers/products");
+ const { getProducts, createProduct } = require("../handlers/products");
 
 const productsRouter = Router();
 
 productsRouter.get("/", getProductsFromDb);
 
 productsRouter.post("/create", postProduct);
-// productsRouter.get("/:idProduct", getById);
+ productsRouter.get("/:idProduct", getById);
 productsRouter.post("/create2", createProductCtrl);
 //
 module.exports = productsRouter;
