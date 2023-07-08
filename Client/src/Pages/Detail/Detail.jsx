64cc0c10e96a0163a47e71d0be2d47335654cfe6
unknown <danosblizzard@hotmail.com>
2023-07-08 06:41:56 -0600

    Detailvista conexion con base de datos se agrego tallas stock

--- Client/src/Pages/Detail/Detail.jsx
+++ Client/src/Pages/Detail/Detail.jsx
@@ -1,40 +1,51 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
+import { deleteProduct, getCartProducts, getTotalCartProducts } from "../../features/cartSlice";
 
-
+import{fetchProducts,productsSlice } from "../../features/productsSlice"
 
 import heartImage from './imagenes/bx-heart.svg.jpg';
 
 const GET_URL = "http://localhost:3001/products";
 
+
+
 function Detail(clickHandler) {
-  const { id } = useParams();
-  const [productData, setProductData] = useState(null);
-  const [selectedImage, setSelectedImage] = useState(null);
+    const { id } = useParams();
+    const [productData, setProductData] = useState(null);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedSize, setSelectedSize] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`${GET_URL}/${id}`);
-        const data = await response.json();
-        setProductData(data);
-        setSelectedImage(data.images[0].imageUrl); // Establecer la primera imagen como seleccionada inicialmente
-      } catch (error) {
-        console.error("Error fetching product data:", error);
-      }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`${GET_URL}/${id}`);
+                const data = await response.json();
+                setProductData(data);
+                setSelectedImage(data.images[0].imageUrl); // Establecer la primera imagen como seleccionada inicialmente
+            } catch (error) {
+                console.error("Error fetching product data:", error);
+            }
+        };
+        
+        fetchData();
+    }, [id]);
+    
+    const handleImageClick = (imageUrl) => {
+        setSelectedImage(imageUrl);
     };
+    
+    const sizes = productData?.sizes?.map((sizeObj) => sizeObj.size) || [];
 
-    fetchData();
-  }, [id]);
+    const handleSizeClick = (size) => {
+        setSelectedSize(size);
+      };
 
-  const handleImageClick = (imageUrl) => {
-    setSelectedImage(imageUrl);
-  };
+    return (
 
-  return (
-    <>
+        <>
       {productData ? (
-        <div className="container mx-auto">
+          <div className="container mx-auto">
           <div className="overflow-hidden bg-white flex flex-row justify-between relative w-full items-center pl-16 pr-[210px]">
             <div className="min-w-[520px]  bg-cover bg-50%_50% bg-blend-normal flex flex-col justify-end relative h-[725px] w-[650px] items-center my-12 py-6 ">
               <img src={selectedImage} alt="Shoes" className="object-cover w-full h-full" style={{ objectFit: 'contain' }} />
@@ -70,57 +81,55 @@ function Detail(clickHandler) {
 
               <div className="text-center font-['Inter'] tracking-[-0.09600000381469727] leading-[24px] text-[#242c31] mb-1 relative w-8">
                 Size
+                </div>
+              <div className="flex flex-row justify-start mb-8 relative items-center space-x-2">
+                {sizes.map((size, index) => (
+                  <div
+                    key={index}
+                    className={`hover:bg-red-200 cursor-pointer overflow-hidden bg-[#f6f7f8] flex flex-col justify-start relative w-12 shrink-0 h-10 items-center py-2 rounded-tr-lg rounded-br-lg border border-gray-300 transition-colors ${
+                      selectedSize === size ? "bg-blue-200" : ""
+                    }`}
+                    onClick={() => handleSizeClick(size)}
+                  >
+                    <div className="text-center text-sm font-['Inter'] tracking-[-0.0840000033378601] leading-[24px] text-[#242c31] relative w-5">
+                      {size}
+                    </div>
+                  </div>
+                ))}
               </div>
-              <div className="flex flex-row justify-start mb-8 relative items-center">
-                <div className="overflow-hidden bg-[#e5e8ea] flex flex-col justify-start relative w-12 shrink-0 h-10 items-center py-2 roundedtl roundedbl cursor-pointer">
-                  <div className="text-center text-sm font-['Inter'] font-semibold tracking-[-0.0840000033378601] leading-[24px] text-[#242c31] relative w-5">
-                    36
-                  </div>
-                </div>
-                <div className="cursor-pointer overflow-hidden bg-[#f6f7f8] flex flex-col justify-start relative w-10 shrink-0 h-10 items-center py-2">
-                  <div className="text-center text-sm font-['Inter'] tracking-[-0.0840000033378601] leading-[24px] text-[#242c31] relative w-2">
-                    40
-                  </div>
-                </div>
-                <div className="cursor-pointer overflow-hidden bg-[#f6f7f8] flex flex-col justify-start relative w-12 shrink-0 h-10 items-center py-2">
-                  <div className="text-center text-sm font-['Inter'] tracking-[-0.0840000033378601] leading-[24px] text-[#242c31] relative w-3">
-                    42
-                  </div>
-                </div>
-                <div className="cursor-pointer overflow-hidden bg-[#f6f7f8] flex flex-col justify-start relative w-10 shrink-0 h-10 items-center py-2 roundedtr roundedbr">
-                  <div className="text-center text-sm font-['Inter'] tracking-[-0.0840000033378601] leading-[24px] text-[#242c31] relative w-2">
-                    38
-                  </div>
-                </div>
+<div className="whitespace-nowrap text-sm font-['Inter'] tracking-[-0.0840000033378601] leading-[24px] text-[#6e7c86] mb-5 relative w-20">
+               Stock {productData.totalStock}
               </div>
+              
+
               <div className="self-stretch flex flex-row justify-start gap-5 relative items-center mb-3 mr-12">
-                <div className="bg-black cursor-pointer flex flex-col justify-center relative w-1/2 h-10 items-center rounded-lg">
-                  <div className="whitespace-nowrap text-sm font-['Inter'] font-semibold tracking-[-0.0840000033378601] text-white relative w-20">
+                <button onClick={() => clickHandler(id,selectedImage)} className="bg-black cursor-pointer flex flex-col justify-center relative w-1/2 h-10 items-center rounded-lg">
+                  <span className="whitespace-nowrap text-sm font-['Inter'] font-semibold tracking-[-0.0840000033378601] text-white relative w-20">
                     Add to cart
-                  </div>
-                </div>
+                  </span>
+                </button>
                 <div className="border-solid border-[#0a0a0a] cursor-pointer flex flex-row justify-center gap-1 relative w-1/5 h-10 items-center border rounded-lg">
                   <div className="text-sm font-['Inter'] font-semibold tracking-[-0.0840000033378601] text-black relative w-12 shrink-0">
                     Favorite
                   </div>
                   <img
                     src={heartImage}
-                    className="min-h-0 min-w-0 relative w-4 h-4 shrink-0 alt=Imagen"
+                    className="min-h-0 min-w-0 relative w-5 h-5 shrink-0 ml-2 alt=Imagen"
                   />
                 </div>
               </div>
-              <div className="whitespace-nowrap text-sm font-['Inter'] tracking-[-0.0840000033378601] leading-[24px] text-[#252c32] self-stretch justify-start mr-40 relative">
-                Shipping*
+              <div className="whitespace-nowrap text-sm font-['Inter'] tracking-[-0.0840000033378601] leading-[24px] text-[#252c32] self-stretch justify-start mr-40 relative ">
+                {/* Shipping* */}
                 <br />
-                To get accurate shipping information{" "}
-                <div className="text-sm font-['Inter'] tracking-[-0.0840000033378601] leading-[24px] text-[#252c32] contents">
-                  Edit Location
+                {/* To get accurate shipping information{" "} */}
+                <div className="text-sm font-['Inter'] tracking-[-0.0840000033378601] leading-[24px] text-[#252c32] contents ">
+                  {/* Edit Location */}
                 </div>
               </div>
             </div>
           </div>
-          <p>Si funcionamos</p>
-          <div>Detail</div>
+          <p></p>
+          <div></div>
         </div>
       ) : (
         <p>Loading...</p>
--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,98 +1,94 @@
 import { GiRunningShoe } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
-import { SiReebok, SiNike } from 'react-icons/si'
-import { CgAdidas } from 'react-icons/cg'
+import { SiReebok, SiNike } from 'react-icons/si';
+import { CgAdidas } from 'react-icons/cg';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
+const GET_URL = "http://localhost:3001/products";
+
 const Home = () => {
-    return (
-        <div className=''>
-            {/* carrousel
-        falta link por id de producto y 
-    cambiar las img por las de la base de dator */}
+  const [carouselImages, setCarouselImages] = useState([4]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-            <div className="carousel rounded-box mt-16">
+  useEffect(() => {
+    fetchCarouselImages();
 
-                <div className="carousel-item max-w-xs max-h-96 object-cover ">
-                    <img src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/816e676888404f6db541aed30115eaed_9366/Zapatillas_adidas_4D_Krazed_Negro_GX9603_01_standard.jpg" alt="Burger" />
-                </div>
-                <div className="carousel-item  max-w-xs max-h-96	 object-cover ">
-                    <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/49413ab1-a863-417a-b0e8-1476c3a204cb/air-max-pulse-womens-shoes-TLnkLm.png" alt="Burger" />
-                </div>
-                <div className="carousel-item max-w-xs max-h-96 object-cover ">
-                    <img src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/bc4b52ea20944a4c8e56ae750169a021_9366/Zapatillas_Galaxy_6_Negro_GW4131_01_standard.jpg" alt="Burger" />
-                </div>
-                <div className="carousel-item max-w-xs max-h-96 object-cover ">
-                    <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/b3c9aea0-80b6-4858-94c9-741b6638f929/tech-hera-womens-shoes-NjvkxR.png" alt="Burger" />
-                </div>
-                <div className="carousel-item max-w-xs max-h-96 object-cover ">
-                    <img src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/a3a6851c80d94bfba393af5b011f795c_9366/Zapatillas_Adifom_Q_Verde_GW2218_01_standard.jpg" alt="Burger" />
-                </div>
-                <div className="carousel-item max-w-xs max-h-96 object-cover ">
-                    <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/d5e9d36d-c1c7-4298-b0e8-720c4cc33280/air-max-90-se-womens-shoes-2ZsM2w.png" alt="Burger" />
-                </div>
-                <div className="carousel-item max-w-xs max-h-96 object-cover ">
-                    <img src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ed861b22275b4b42a62daf460039a6b1_9366/Zapatillas_Zntasy_Lightmotion_Beige_HP6667_01_standard.jpg" alt="Burger" />
-                </div>
-            </div>
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % carouselImages.length);
+    }, 1000);
 
-            <div>
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [carouselImages]);
 
+  const fetchCarouselImages = async () => {
+    try {
+      const response = await axios.get(GET_URL);
+      const products = response.data;
+      const firstImages = products.map((product) => product.images[0].imageUrl);
+      setCarouselImages(firstImages);
+    } catch (error) {
+      console.error("Error fetching carousel images:", error);
+    }
+  };
 
-            </div>
+  return (
+    <div>
+      <div className="carousel rounded-box mt-16">
+        {carouselImages.map((image, index) => {
+          const productId = index + 1; // Asignar el ID del producto
+          return (
+            <Link key={index} to={`/home/${productId}`} className={`carousel-item max-w-xs max-h-96 object-cover ${index === currentImageIndex ? 'active' : ''}`}>
+              <img src={image} alt="Product" />
+            </Link>
+          );
+        })}
+      </div>
 
-            {/* //title// */}
-            <div className='m-6 mt-9 mb-9 text-center'>
-                <h1>STEP-BY-STEP</h1>
-                <p style={{ display: 'inline-flex', alignItems: 'center' }}>
-                    todo lo que buscas en zapas <GiRunningShoe className="ml-1" />
-                </p>
-                <div className="flex justify-center mt-4">
-                    <Link to="/tienda"><button className="bg-black text-white py-2 px-4 rounded hover:border-gray-500 hover:bg-gray-500">
-                        COMPRAR
-                    </button>
-                    </Link>
-                </div>
-            </div>
-
-
-            {/* banner 
-    agregar 
--Link con filtros por marca*/}
-            <div className="flex justify-center">
-                <div className="w-1/3 h-96 overflow-hidden relative">
-                    <img src="https://reebokarg.vtexassets.com/assets/vtex.file-manager-graphql/images/60761dab-3616-4721-902b-2e1d09142f19___ed7c12cfa15a0ddde6215f0f812f93f2.jpg" className="w-full h-full object-cover" />
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center tooltip" data-tip="Reebok">
-                        <button className="text-4xl px-8 py-2 bg-black inline-flex items-center text-white font-bold rounded-full shadow-lg hover:bg-gray-950 hover:border-slate-200 hover:border-2  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1) duration-400 space-x-2">
-                            <SiReebok />
-
-
-                        </button>
-                    </div>
-                </div>
-                <div className="w-1/3 h-96 overflow-hidden relative">
-                    <img src="https://media.about.nike.com/images/8221b9c2-3f4e-4864-9a50-c760d336d8e2/5.jpg?fm=jpg&q=80&fit=max&crop=2400%2C3000%2C0%2C0&w=640" className="w-full h-full object-cover" />
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center tooltip" data-tip="Nike">
-                        <button className="text-4xl px-8 py-2 bg-black inline-flex items-center text-white font-bold rounded-full shadow-lg hover:bg-gray-950 hover:border-slate-200 hover:border-2  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1) duration-400 space-x-2">
-
-                            <SiNike />
-                        </button>
-                    </div>
-
-                </div>
-                <div className="w-1/3 h-96 overflow-hidden relative">
-                    <img src="https://media.revistagq.com/photos/5e0605b0aec425000859123f/master/w_1600%2Cc_limit/unnamed.jpg" className="w-full h-full object-cover" />
-                    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center tooltip" data-tip="Adidas">
-                        <button className="text-4xl px-8 py-2 bg-black inline-flex items-center text-white font-bold rounded-full shadow-lg hover:bg-gray-950 hover:border-slate-200 hover:border-2  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1) duration-400 space-x-2">
-
-                            <CgAdidas />
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-
+      <div className="m-6 mt-9 mb-9 text-center">
+        <h1>STEP-BY-STEP</h1>
+        <p style={{ display: 'inline-flex', alignItems: 'center' }}>
+          todo lo que buscas en zapas <GiRunningShoe className="ml-1" />
+        </p>
+        <div className="flex justify-center mt-4">
+          <Link to="/tienda">
+            <button className="bg-black text-white py-2 px-4 rounded hover:border-gray-500 hover:bg-gray-500">
+              COMPRAR
+            </button>
+          </Link>
         </div>
+      </div>
 
+      <div className="flex justify-center">
+        <div className="w-1/3 h-96 overflow-hidden relative">
+          <img src="https://reebokarg.vtexassets.com/assets/vtex.file-manager-graphql/images/60761dab-3616-4721-902b-2e1d09142f19___ed7c12cfa15a0ddde6215f0f812f93f2.jpg" className="w-full h-full object-cover" alt="Reebok" />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center tooltip" data-tip="Reebok">
+            <button className="text-4xl px-8 py-2 bg-black inline-flex items-center text-white font-bold rounded-full shadow-lg hover:bg-gray-950 hover:border-slate-200 hover:border-2  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1) duration-400 space-x-2">
+              <SiReebok />
+            </button>
+          </div>
+        </div>
+        <div className="w-1/3 h-96 overflow-hidden relative">
+          <img src="https://media.about.nike.com/images/8221b9c2-3f4e-4864-9a50-c760d336d8e2/5.jpg?fm=jpg&q=80&fit=max&crop=2400%2C3000%2C0%2C0&w=640" className="w-full h-full object-cover" alt="Nike" />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center tooltip" data-tip="Nike">
+            <button className="text-4xl px-8 py-2 bg-black inline-flex items-center text-white font-bold rounded-full shadow-lg hover:bg-gray-950 hover:border-slate-200 hover:border-2  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1) duration-400 space-x-2">
+              <SiNike />
+            </button>
+          </div>
+        </div>
+        <div className="w-1/3 h-96 overflow-hidden relative">
+          <img src="https://media.revistagq.com/photos/5e0605b0aec425000859123f/master/w_1600%2Cc_limit/unnamed.jpg" className="w-full h-full object-cover" alt="Adidas" />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center tooltip" data-tip="Adidas">
+            <button className="text-4xl px-8 py-2 bg-black inline-flex items-center text-white font-bold rounded-full shadow-lg hover:bg-gray-950 hover:border-slate-200 hover:border-2  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1) duration-400 space-x-2">
+              <CgAdidas />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    )
-}
 export default Home;
