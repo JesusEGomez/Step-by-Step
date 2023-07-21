560dfb4bb4787120cf83e57851221460c8161530
lgramajo99 <lucianongramajo@gmail.com>
2023-07-21 11:30:19 -0300

    funcionalidad de agregar o sacar administrador listo.

--- Api/src/controllers/products/productUpdateCtrl.js
+++ Api/src/controllers/products/productUpdateCtrl.js
@@ -51,9 +51,7 @@ const updateProductCtrl = async (req, res) => {
     res.status(200).json(updatedProduct);
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({ message: "Error al actualizar el producto", error: error.message });
+    res.status(500).json({ message: "Error al actualizar el producto", error: error.message });
   }
 };
 
--- Api/src/controllers/users/updateUserCtrl.js
+++ Api/src/controllers/users/updateUserCtrl.js
@@ -3,6 +3,7 @@ const { findById } = require("../../utils/findBy.js");
 
 const updateUserCtrl = async (req, res) => {
   const { id } = req.params;
+  const { isAdmin } = req.body
   try {
     // Obtengo todos los datos del usuario
     const user = await findById(User, id);
@@ -11,16 +12,19 @@ const updateUserCtrl = async (req, res) => {
       return res.status(404).json({ message: `No hay un usuario registrado con este id` });
     }
 
+    user.isAdmin = isAdmin;
     // si encuentra el usuario, se realiza el cambio con la informacion entrante.
-    await user.update({
-      // name,
-      // lastname,
-      // user,
-      // mail,
-      // phone,
-      isAdmin,
-    });
-    return res.status(200).json({ message: `Solicitud de cambio con exito.`, user });
+    // await user.update({
+    //   // name,
+    //   // lastname,
+    //   // user,
+    //   // mail,
+    //   // phone,
+    //   isAdmin,
+    // });
+    await user.save();
+
+    return res.status(200).json({ message: `Solicitud de cambio con exito.`, usuario: user });
 
   } catch (error) {
     return res.status(500).json({ message: "Hubo un problema al procesar la solicitud", error: error.message, });
