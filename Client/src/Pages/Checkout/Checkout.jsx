3aa3d82dd22548dc3c9046b4dc81126223be10af
reyregio <marisafenogliomnf@gmail.com>
2023-07-20 21:41:11 -0300

    me ovide de la bd

--- Client/src/Pages/Checkout/Checkout.jsx
+++ Client/src/Pages/Checkout/Checkout.jsx
@@ -22,65 +22,7 @@ const Checkout = () => {
     return (
         <div className="flex items-center justify-center w-full h-full mt-32 bg-gray-100">
             <div className="bg-white rounded-lg shadow-lg p-8 w-[90%] mx-4 md:mx-auto md:flex">
-                {/* ///Form posible
-        <div className="md:w-2/3 md:mr-8">
-        <div className="flex flex-col w-full border-opacity-50">
-        <div className="grid h-[20%] card bg-gray-50 p-6 rounded-box place-items-center">
-        <h2 className="text-2xl font-medium not-italic  font-sans text-gray-700	mb-4">Información de Envío</h2>
-        
-        <button className="bg-gray-800 p-6 hover:bg-gray-700 hover:border-gray-500 hover:border-2 text-white font-bold py-2 px-4 rounded w-full mb-4 flex items-center justify-center">
-        Iniciar Sesión <HiMiniArrowLongRight className="ml-1 w-6 h-auto" />
-        </button>
-        </div>
-        <div className="divider">Ò</div>
-        <div className="grid h-[100%] card bg-gray-50 p-6 rounded-box place-items-center">
-        <div className="space-x-2 pb-4 w-[90%] p-10 mb-4">
-        <h3 className="text-lg font-bold mb-2">Dirección de Envío</h3>
-        <form>
-        <div className="mb-4">
-        <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
-        Email
-        </label>
-        <input
-        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="email"
-        type="email"
-        placeholder="Email"
-        />
-        </div>
-        <div className="mb-4">
-        <label className="block text-gray-700 font-bold mb-2" htmlFor="address">
-        Dirección
-        </label>
-        <input
-        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="address"
-        type="text"
-        placeholder="Dirección"
-        />
-        </div>
-        <div className="mb-4">
-        <label className="block text-gray-700 font-bold mb-2" htmlFor="province">
-        Provincia
-        </label>
-        <input
-        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        id="province"
-        type="text"
-        placeholder="Provincia"
-        />
-        </div>
-        
-        <button className="bg-black text-white font-bold py-2 px-4 rounded w-full">
-        Registrar
-        </button>
-        
-        </form>
-        </div>
-        </div>
-        </div> 
-    </div>*/}
-
+                
                 <div className="md:w-full">
                     <h2 className="text-2xl font-bold mb-4 m-3">Carrito</h2>
                     <div className="bg-white rounded-lg shadow-md p-4 mb-4">
