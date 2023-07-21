555aa4c086b70afe62ae14eaa1b217239e183d8b
reyregio <marisafenogliomnf@gmail.com>
2023-07-20 21:31:28 -0300

    dashboar sin logica de products
    orders ready, estilos ready, ispublic? readyn't

--- Client/src/Pages/Dashboard/Orders.jsx
+++ Client/src/Pages/Dashboard/Orders.jsx
@@ -1,86 +1,49 @@
-import * as React from 'react';
-import Link from '@mui/material/Link';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
+import React, { useEffect, useState } from 'react';
 import Title from './Title';
+import ordersSlice, {fetchOrders, getOrders} from '../../features/ordersSlice'
+import { useDispatch, useSelector } from 'react-redux';
+import { DataGrid } from '@mui/x-data-grid';
 
-// Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-    return { id, date, name, shipTo, paymentMethod, amount };
-}
 
-const rows = [
-    createData(
-        0,
-        '16 Mar, 2019',
-        'Elvis Presley',
-        'Tupelo, MS',
-        'VISA ⠀•••• 3719',
-        312.44,
-        ),
-        createData(
-            1,
-            '16 Mar, 2019',
-            'Paul McCartney',
-            'London, UK',
-            'VISA ⠀•••• 2574',
-            866.99,
-            ),
-            createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-            createData(
-                3,
-                '16 Mar, 2019',
-                'Michael Jackson',
-                'Gary, IN',
-                'AMEX ⠀•••• 2000',
-                654.39,
-                ),
-                createData(
-                    4,
-                    '15 Mar, 2019',
-                    'Bruce Springsteen',
-                    'Long Branch, NJ',
-                    'VISA ⠀•••• 5919',
-                    212.79,
-                    ),
-                ];
-                
-                function preventDefault(event) {
-                    event.preventDefault();
-                }
-                
-                export default function Orders() {
-                    return (
-                        <React.Fragment>
-                        <Title>Ultimas Ordenes</Title>
-                        <Table size="small">
-                        <TableHead>
-                        <TableRow>
-                        <TableCell>Date</TableCell>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Ship To</TableCell>
-                        <TableCell>Payment Method</TableCell>
-                        <TableCell align="right">Sale Amount</TableCell>
-                        </TableRow>
-                        </TableHead>
-                        <TableBody>
-                        {rows.map((row) => (
-                            <TableRow key={row.id}>
-                            <TableCell>{row.date}</TableCell>
-                            <TableCell>{row.name}</TableCell>
-                            <TableCell>{row.shipTo}</TableCell>
-                            <TableCell>{row.paymentMethod}</TableCell>
-                            <TableCell align="right">{`$${row.amount}`}</TableCell>
-                            </TableRow>
-                            ))}
-                            </TableBody>
-                            </Table>
-                            <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-                            See more orders
-                            </Link>
-                            </React.Fragment>
-                            );
-                        }
+
+
+
+export default function Orders() {
+const dispatch=useDispatch()
+const orders =useSelector(getOrders);
+useEffect(()=>{
+    dispatch(fetchOrders())
+}, [dispatch])
+
+console.log(orders)
+
+const rows = orders.map((order)=>({
+    id: order.id,
+    col1: order.id,
+    col2: order.orderNumber,
+    col3: order.paymentStatus,
+    col4: order.fullFillmentStatus,
+    col5:order.email
+}))
+
+const columns=[
+    { field:'col1', headerName:"ID", width: 250 },
+    { field:'col2', headerName:"Numero de orden", width: 150 },
+    { field:'col3', headerName:"Estado del pago ", width: 150 },
+    { field:'col4', headerName:"Envio", width: 150 },
+    { field:'col5', headerName:"Email", width: 350 },
+
+]
+
+    return (
+        <React.Fragment>
+        
+        <div style={{height:'20%',width:'100%'}}>
+        <Title>Ultimas Ordenes</Title>
+        <DataGrid rows={rows} columns={columns} />
+
+
+        </div>
+        </React.Fragment>
+        );
+    }
--- Client/src/Pages/Dashboard/Products.jsx
+++ Client/src/Pages/Dashboard/Products.jsx
@@ -3,113 +3,107 @@ import { getAllProducts, fetchProducts } from '../../features/productsSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { DataGrid } from '@mui/x-data-grid';
 import Switch from '@mui/material/Switch';
-
+import Title from './Title';
 
 function renderImageCell(params) {
     const imageUrls = params.value.split(', ');
     return (
-        <div>
-        {imageUrls.length > 0 ? (
-            imageUrls.map((url, index) => (
-                <img
-                key={index}
-                src={url}
-                alt={`Image ${index}`}
-                style={{ width: 30, height: 30, padding: 2, marginRight: 5 }}
-                />
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+            {imageUrls.length > 0 ? (
+                imageUrls.map((url, index) => (
+                    <img
+                        key={index}
+                        src={url}
+                        alt={`Image ${index}`}
+                        style={{ width: 55, height: 55, padding: 2, marginRight: 5 }}
+                    />
                 ))
-                ) : (
-                    <span>No Images</span>
-                    )}
-                    </div>
-                    );
-                }
-                function renderSwitchCell(params) {
-                    const isPublish = params.row.col6 === 'Yes';
-                    
-                    const handleChange = (event) => {
-                        // Add your logic here to update the 'isPublish' field in your data source
-                        const updatedRows = rows.map((row) => {
-                            if (row.id === params.row.id) {
-                                return { ...row, col6: event.target.checked ? 'Yes' : 'No' };
-                            }
-                            return row;
-                        });
-                        
-                        // Call any function or dispatch any action needed to update the Redux store or your data source
-                        console.log('Switch value changed:', event.target.checked);
-                        // Example dispatching action to update the Redux store (if you're using Redux)
-                        // dispatch(updateProduct(params.row.id, { isPublish: event.target.checked ? 'Yes' : 'No' }));
-                        
-                        // Optional: Set the updated rows state to trigger a re-render of the DataGrid
-                        setRows(updatedRows);
-                    };
-                    
-                    return <Switch checked={isPublish} onChange={handleChange} />;
-                }
-                
-                
-                function Products() {
-                    const dispatch = useDispatch();
-                    const products = useSelector(getAllProducts);
-                    const [loading, setLoading] = useState(true);
-                    
-                    useEffect(() => {
-                        dispatch(fetchProducts())
-                        .then(() => {
-                            setLoading(false);
-                        })
-                        .catch((error) => {
-                            console.error('Error fetching data:', error);
-                            setLoading(false);
-                        });
-                    }, [dispatch]);
-                    
-                    const rows = products.map((product) => ({
-                        id: product.id,
-                        col1: product.id,
-                        col2: product.item_number,
-                        col3: product.model,
-                        col4: product.description,
-                        col5: product.discountPercentage,
-                        col6: product.isPublish ? 'Yes' : 'No',
-                        col7: product.sold_count,
-                        col8: product.gender,
-                        col9: product.images.join(', '),
-                        col10: product.brand, 
-                        col11: product.colors, 
-                        col12: product.categories.join(', '), 
-                        col13: product.stock.map((size) => size.size).join(', '),
-                        col14: product.stock.reduce((totalStock, size) => totalStock + size.stockPerSize, 0),
-                    }));
-                    
-                    const columns = [
-                        { field: 'col1', headerName: 'Id', width: 50 },
-                        { field: 'col2', headerName: 'Item_Number', width: 150 },
-                        { field: 'col3', headerName: 'Nombre', width: 350 },
-                        { field: 'col4', headerName: 'Descripcion', width: 150 },
-                        { field: 'col5', headerName: 'Porcentaje de Descuento', width: 50 },
-                        { field: 'col6', headerName: 'Publicado', width: 150, renderCell: renderSwitchCell, },
-                        { field: 'col7', headerName: 'Contador de ventas', width: 100 },
-                        { field: 'col8', headerName: 'Generos', width: 150 },
-                        { field: 'col9', headerName: 'Imagenes', width: 100, renderCell: renderImageCell },
-                        { field: 'col10', headerName: 'Marca', width: 100 },
-                        { field: 'col11', headerName: 'Color', width: 100 },
-                        { field: 'col12', headerName: 'Categorias', width: 150 },
-                        { field: 'col13', headerName: 'Talles', width: 400 },
-                        { field: 'col14', headerName: 'Stock', width: 150 },
-                    ];
-                    
-                    return (
-                        <div style={{ height: '100%', width: '100%' }}>
-                        {loading ? (
-                            <span className="loading loading-bars ml-96 mt-28 loading-3xl"></span>
-                            ) : (
-                                <DataGrid rows={rows} columns={columns} />
-                                )}
-                                </div>
-                                );
-                            }
-                            
-                            export default Products;
-                            
+            ) : (
+                <span>No Images</span>
+            )}
+        </div>
+    );
+}
+function renderSwitchCell(params) {
+    const isPublish = params.row.col6 === 'Yes';
+
+    const handleChange = (event) => {
+        const updatedRows = rows.map((row) => {
+            if (row.id === params.row.id) {
+                return { ...row, col6: event.target.checked ? 'Yes' : 'No' };
+            }
+            return row;
+        });
+
+        console.log('Switch value changed:', event.target.checked);
+        setRows(updatedRows);
+    };
+
+    return <Switch checked={isPublish} onChange={handleChange} />;
+}
+
+
+function Products() {
+    const dispatch = useDispatch();
+    const products = useSelector(getAllProducts);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        dispatch(fetchProducts())
+            .then(() => {
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            });
+    }, [dispatch]);
+
+    const rows = products.map((product) => ({
+        id: product.id,
+        col1: product.id,
+        col2: product.item_number,
+        col3: product.model,
+        col4: product.description,
+        col5: product.discountPercentage,
+        col6: product.isPublish ? 'Yes' : 'No',
+        col7: product.sold_count,
+        col8: product.gender,
+        col9: product.images.join(', '),
+        col10: product.brand,
+        col11: product.colors,
+        col12: product.categories.join(', '),
+        col13: product.stock.map((size) => size.size).join(', '),
+        col14: product.stock.reduce((totalStock, size) => totalStock + size.stockPerSize, 0),
+    }));
+
+    const columns = [
+        { field: 'col1', headerName: 'Id', width: 50 },
+        { field: 'col2', headerName: 'Item_Number', width: 150 },
+        { field: 'col3', headerName: 'Nombre', width: 350 },
+        { field: 'col4', headerName: 'Descripcion', width: 150 },
+        { field: 'col5', headerName: 'Porcentaje de Descuento', width: 50 },
+        { field: 'col6', headerName: 'Publicado', width: 150, renderCell: renderSwitchCell, },
+        { field: 'col7', headerName: 'Contador de ventas', width: 100 },
+        { field: 'col8', headerName: 'Generos', width: 100 },
+        { field: 'col9', headerName: 'Imagenes', width: 250, renderCell: renderImageCell },
+        { field: 'col10', headerName: 'Marca', width: 100 },
+        { field: 'col11', headerName: 'Color', width: 100 },
+        { field: 'col12', headerName: 'Categorias', width: 150 },
+        { field: 'col13', headerName: 'Talles', width: 400 },
+        { field: 'col14', headerName: 'Stock', width: 150 },
+    ];
+
+    return (
+        <div style={{ height: '95%', width: '100%' }}>
+            <Title>Productos</Title>
+            {loading ? (
+                <span className="loading loading-bars ml-96 mt-28 loading-3xl"></span>
+            ) : (
+                <DataGrid rows={rows} columns={columns} />
+            )}
+        </div>
+    );
+}
+
+export default Products;
