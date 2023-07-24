2569b6cba578fe5ccc3a562752d32235e7c8823b
JesusEGomez <111249837+JesusEGomez@users.noreply.github.com>
2023-07-24 11:12:49 -0300

    Update users.slice.js

--- Client/src/features/users.slice.js
+++ Client/src/features/users.slice.js
@@ -35,7 +35,7 @@ export const addNewUsers = createAsyncThunk(
 export const updateUser = createAsyncThunk("users/updateUsers",
   async ({ id, data }) => {
     try {
-      const response = await axios.put(`http://localhost:3001/users/${id}`, data);
+      const response = await axios.put(${URL}/users/${id}, data);
       const updatedUser = response.data;
       return updatedUser;
 
