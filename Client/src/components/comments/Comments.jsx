3d7778437ff9b3c2960372b9b119d74e843b7687
TomasHas <thastrup@gmail.com>
2023-07-23 21:05:55 -0300

    fixes botones home

--- Client/src/components/comments/Comments.jsx
+++ Client/src/components/comments/Comments.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { getOrders } from "../../features/ordersSlice";
 
 const URL = import.meta.env.VITE_URL;
-const INTERVAL_DELAY = 2000;
+const INTERVAL_DELAY = 200000;
 
 function Comments() {
   // const user = JSON.parse(localStorage.getItem("user"));
@@ -22,17 +22,24 @@ function Comments() {
 
   // console.log("hasOrder", hasOrder, hasComment);
   useEffect(() => {
-    const findHasComment = comments?.find((c) => c.mail === user?.mail);
+    const findHasOrder = orders
 
-    // console.log("findHasComment", findHasComment);
-    if (findHasComment) {
-      setHasComment(true);
-    }
-    const findHasOrder = orders?.find(
-      (o) => o.email === user?.email && o.paymentStatus === "approved"
-    );
+      ?.map((o) => o.paymentStatus === "approved" && o.email)
+
+      .includes(user.email);
     if (findHasOrder) {
       setHasOrder(true);
+    }
+
+    // console.log("findHasOrder", findHasOrder);
+    // console.log("orders", orders);
+
+    const findHasComment = comments
+      ?.map((c) => c.user.mail)
+      .includes(user.email);
+
+    if (findHasComment) {
+      setHasComment(true);
     }
   }, [comments, orders, user]);
   // console.log("user.email", user.email);
