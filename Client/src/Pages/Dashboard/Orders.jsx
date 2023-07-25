41f855af70d1d1e80f0d81ecf373fb9b92ba4b0f
reyregio <marisafenogliomnf@gmail.com>
2023-07-25 20:00:03 -0300

    estilos y claudinary arreglado
    cositas del estilos del dashboard

--- Client/src/Pages/Dashboard/Orders.jsx
+++ Client/src/Pages/Dashboard/Orders.jsx
@@ -9,41 +9,41 @@ import { DataGrid } from '@mui/x-data-grid';
 
 
 export default function Orders() {
-const dispatch=useDispatch()
-const orders =useSelector(getOrders);
-useEffect(()=>{
-    dispatch(fetchOrders())
-}, [dispatch])
-
-console.log(orders)
-
-const rows = orders.map((order)=>({
-    id: order.id,
-    col1: order.id,
-    col2: order.orderNumber,
-    col3: order.paymentStatus,
-    col4: order.fullFillmentStatus,
-    col5:order.email
-}))
-
-const columns=[
-    { field:'col1', headerName:"ID", width: 250 },
-    { field:'col2', headerName:"Numero de orden", width: 150 },
-    { field:'col3', headerName:"Estado del pago ", width: 150 },
-    { field:'col4', headerName:"Envio", width: 150 },
-    { field:'col5', headerName:"Email", width: 350 },
-
-]
-
-    return (
-        <React.Fragment>
+    const dispatch=useDispatch()
+    const orders =useSelector(getOrders);
+    useEffect(()=>{
+        dispatch(fetchOrders())
+    }, [dispatch])
+    
+    console.log(orders)
+    
+    const rows = orders.map((order)=>({
+        id: order.id,
+        col1: order.id,
+        col2: order.orderNumber,
+        col3: order.paymentStatus,
+        col4: order.fullFillmentStatus,
+        col5:order.email
+    }))
+    
+    const columns=[
+        { field:'col1', headerName:"ID", width: 250 },
+        { field:'col2', headerName:"Numero de orden", width: 150 },
+        { field:'col3', headerName:"Estado del pago ", width: 150 },
+        { field:'col4', headerName:"Envio", width: 150 },
+        { field:'col5', headerName:"Email", width: 250 },
         
-        <div style={{height:'20%',width:'100%'}}>
+    ]
+    
+    return (
+        <div style={{ backgroundColor: 'white', height: '70%', width: '110%',padding:'20px'  }}>
+        
+        <div style={{height:'90%',width:'98%'  ,marginLeft:'20px',marginRight:'20px'}}>
         <Title>Ultimas Ordenes</Title>
         <DataGrid rows={rows} columns={columns} />
-
-
+        
+        
         </div>
-        </React.Fragment>
+        </div>
         );
     }
